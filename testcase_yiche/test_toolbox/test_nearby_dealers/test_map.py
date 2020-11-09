d3b2972c5e3ebc6d1ead45f798bb7f291e82670d
yang <yangcx0123@126.com>
2020-11-09 10:41:56 +0800

    易车

--- testcase_yiche/test_toolbox/test_nearby_dealers/test_map.py
+++ testcase_yiche/test_toolbox/test_nearby_dealers/test_map.py
@@ -12,5 +12,6 @@ class TestMap:
         addr = self.main.start().my().more().goto_nearby_dealers().map()
         # 模拟器
         # assert addr == "泉州展览城-东门"
+
         # OPPO
         assert addr == "泉州展览城"
